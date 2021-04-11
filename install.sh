d93708ebef190a8c594723357d021a82ff13bf43
drtychai <drtychai@protonmail.com>
2021-04-11 14:19:37 -0700

    arch compatibility

--- install.sh
+++ install.sh
@@ -3,32 +3,53 @@ set -e
 
 # Colors
 L_GREEN='\033[1;32m'
-L_BLUE='\033[1;34m'
 L_RED='\033[1;31m'
+
+L_GREY='\033[2;49;39m'
+GREY='\033[0;49;90m'
+
+L_BLUE='\033[1;34m'
 BLUE='\033[0;34m'
+
 NC='\033[0m' # No Color
 
 # Globals
-SH=`echo ${SHELL} | cut -d '/' -f3`
-DIR="$(cd "$(dirname "$0")" ; pwd -P)"
+SH="$(echo ${SHELL} | cut -d '/' -f3)"
+DIR="$(pwd -P)"
 
 # Logging
-function info  { echo -e "${L_GREEN}[+]${NC}" $@; }
-function debug { echo -e "${L_BLUE}[*]${NC}" $@; }
-function error { echo -e "${BLUE}[-]${NC}" $@; }
-function fatal { echo -e "${L_RED}[-]${NC}" $@ && exit 1; }
-function prompt  { echo -ne "${L_GREEN}[+]${NC} $@ "; }
+function info    { echo -e "${L_GREEN}[+] $@ ${NC}"; }
+function debug   { echo -e  "${L_GREY}[*] $@ ${NC}"; }
+function error   { echo -e    "${GREY}[-] $@ ${NC}"; }
+function prompt { echo -ne "${L_GREEN}[+] $@ ${NC}"; }
+
+function fatal   { echo -e   "${L_RED}[-] $@ ${NC}" && exit 1; }
+function success { echo -e "${L_GREEN}[+] $@ ${NC}" && exit 0; }
 
 # Create a symbolic link from the file/dir in our repo to
 # the local environment. If file currently exists, attempt to
 # manually unlink performs recursively calls until all links created
 # --> fn args are idential to symlink <SRC> <TRGT>
 function symlink {
-    local src_name=${1}
-    local trgt_name=${2}
-    if ! `ln -s ${src_name} ${trgt_name}`; then
-        debug "Manually unlinking ${trgt_name}..."
-        [ unlink ${trgt_name} ] && return
+    local src_name="${1}"
+    local trgt_name="${2}"
+    [[ "${trgt_name}" == *"install"* ]] && return
+   
+    local buf_space="%$(expr 35 - `echo "${src_name/${DIR}/.}" | wc -c | xargs`)s"
+
+    #debug "Linking: ${src_name##*/} => ${trgt_name/${HOME}/~}"
+    local debug_msg="Linking: "
+    debug_msg="${debug_msg}${src_name/${DIR}/.}"
+    debug_msg="${debug_msg}`printf ${buf_space}`"
+    debug_msg="${debug_msg}=> "
+    debug_msg="${debug_msg}${trgt_name/${HOME}/~}"
+
+    debug ${debug_msg}
+
+    if [[ ! `ln -s ${src_name} ${trgt_name}` ]];then
+        # Error throw in call
+	    #debug "Manually linking ${trgt_name}..." 
+        [[ ! `unlink ${trgt_name}` ]] && ln -s ${src_name} ${trgt_name} 
     fi
 }
 
@@ -37,15 +58,13 @@ function map_local_bins {
     if ! grep -q 'dotfiles/bin' ${HOME}/.${SH}rc; then
     	echo "# Add dotfiles bin to PATH" >> ${HOME}/.${SH}rc
     	echo "export PATH=\${PATH}:${DIR}/bin" >> ${HOME}/.${SH}rc
-    else
-        error "dotfiles bin already in .${SH}rc PATH"
+    #else error "dotfiles bin already in .${SH}rc PATH"
     fi
 
     # Add to path in ${HOME}/.${SH}rc
     if ! grep -q '/usr/local/sbin' ${HOME}/.${SH}rc; then
         echo "export PATH=\$PATH:/usr/local/sbin" >> ${HOME}/.${SH}rc
-    else
-        error "local sbin already in .${SH}rc PATH"
+    #else error "local sbin already in .${SH}rc PATH"
     fi
 }
 
@@ -55,33 +74,44 @@ function map_aliases {
     if ! grep -q bash_aliases ${HOME}/.${SH}rc; then
         echo "# Set up aliases" >> ${HOME}/.${SH}rc
         echo "[ -f \${HOME}/.bash_aliases ] && source \${HOME}/.bash_aliases" >> ${HOME}/.${SH}rc
-    else
-        error ".bash_aliases already in .${SH}rc"
+    #else error ".bash_aliases already in .${SH}rc"
     fi
 
     # Install git aliases
     if ! grep -q gitaliases ${HOME}/.gitconfig 2>/dev/null; then
         echo "[include]" >> ${HOME}/.gitconfig
         echo "	path = ~/.gitaliases" >> ${HOME}/.gitconfig
-    else
-        error ".gitaliases already in .gitconfig"
+    #else error ".gitaliases already in .gitconfig"
     fi
 
-    # Install all rust cli tools, if approved
-    prompt "Install missing rust binaries? [y/N]:"
-    read response
-    case "$response" in
+    # Default to skip installation if timeout is triggered 
+    prompt "Install modern CLI (rust) binaries? [y/N]:"
+    read -t 5 response;
+
+    case "${response}" in
         [yY][eE][sS]|[yY]) 
             if ! grep -q rs-aliases ${HOME}/.cargo/env 2>/dev/null; then
                 echo "source ${DIR}/config/cargo/rs-aliases" >> ${HOME}/.cargo/env
-            else
-                error "rust aliases already in .cargo/env"
+            #else error "rust aliases already in .cargo/env"
             fi
                 
-            local rs_bins="`cat ${DIR}/config/cargo/rs-aliases | cut -d '=' -f 2 | xargs`"
-            for bin in "${rs_bins}";do 
-	        cargo install ${bin}
-	    done
+            declare -a RS_BINS=(
+                "exa"
+                "bat" 
+                "zoxide" 
+                "starship" 
+                "tokei" 
+                "du-dust" 
+                "fd-find" 
+                "sd" 
+                "grex" 
+                "ripgrep"
+                "bottom"
+                "cw"
+            )
+	        cargo install -j`nproc` ${RS_BINS[@]}
+            ;;
+        [nN][oO]|[nN])
             ;;
         *)
             ;;
@@ -90,19 +120,22 @@ function map_aliases {
 
 function map_symlinks {
     # Excluded files
-    declare -a EXCLUDED=("README.md" "bin" "iterm2" "install.sh" "clean.sh")
+    declare -a EXCLUDED=( "README.md" "bin" "iterm2" "install" "clean" )
 
     # Setup symlinks
-    for file in `ls ${DIR}`; do
+    for file in $(ls ${DIR}); do
         IFS=@
-        case "@${EXCLUDED[*]}@" in
+        case "@${EXCLUDED[*]}@" in 
           *"@${file}@"*)
             continue
             ;;
-          *) # current file is not blacklisted; hence we must symlink it
+          *) # current file is not excluded; so we must symlink it
             case ${file} in
               *"config")
                 link_config_to_local
+                ;;
+              *"install"*)
+                continue
                 ;;
               *)
                 # Bail out for empty strings
@@ -116,23 +149,33 @@ function map_symlinks {
             ;;
         esac
    done
+
+   # SymLink dotfiles and configs
+   symlink "${DIR}/bash_aliases" "${HOME}/.bash_aliases"
+   symlink "${DIR}/zshrc" "${HOME}/.zshrc"
 }
 
 function link {
     local filename="$(basename $0)"
     local debug="${DIR}/.debug"
+
     if [[ "${filename}" != "$(basename $file)" ]]; then
         if [ -e ${HOME}/.${filename} ]; then
             if ! [ -h ${HOME}/.${filename} ]; then
                 mkdir -p $debug
                 # Move existing dotfile to $debug
                 mv ${HOME}/.${filename} $debug/
-            else
-                error ".${filename} is already symlinked"
+            #else error ".${filename} is already symlinked"
             fi
         else
-            debug "Creating link for .${filename}"
-            symlink ${DIR}/${filename} ${HOME}/.${filename}
+	    case "${filename}" in
+              *"install"*)
+                  continue
+                  ;;
+              *)
+                  symlink ${DIR}/${filename} ${HOME}/.${filename}
+                  ;;
+            esac
         fi
     fi
 }
@@ -158,12 +201,11 @@ function link_config_to_local {
 
             # Backup pre-exisiting file, if present
             if [ -f "${usr_conf}/${f}/${f}.json" ];then
-                debug "Backing up resource... ${f}.json -> ${f}.json.bak"
+                #debug "Backing up resource... ${f}.json -> ${f}.json.bak"
                 mv "${usr_conf}/${f}/${f}.json" "${usr_conf}/${f}/${f}.json.bak" 
             fi
 
             # Link: keybindings
-            debug "Creating link for ${usr_conf}/${f}/${f}.json"
             symlink "${config_dir}/${f}/${f}.json" "${usr_conf}/${f}/${f}.json"
             ;;
         *"zathura")
@@ -175,22 +217,19 @@ function link_config_to_local {
 
             # Backup pre-exisiting file, if present
             if [ -f "${usr_conf}/${f}/${f}rc" ];then
-                debug "Backing up resource... ${f}rc -> ${f}rc.bak"
+                #debug "Backing up resource... ${f}rc -> ${f}rc.bak"
                 mv "${usr_conf}/${f}/${f}rc" "${usr_conf}/${f}/${f}rc.bak" 
             fi
 
             # Link: keybindings
-            debug "Creating link for ${usr_conf}/${f}/${f}rc"
             symlink "${config_dir}/${f}/${f}rc" "${usr_conf}/${f}/${f}rc"
             ;;
         *"base16_color_space.sh")
             local conf="${usr_conf}/base16_color_space.sh"
-            debug "Creating link for ${conf}"
             symlink ${d} ${conf}
             ;; 
         *"starship.toml")
             local conf="${usr_conf}/starship.toml"
-            debug "Creating link for ${conf}"
             symlink ${d} ${conf}
             ;; 
         *)
@@ -224,18 +263,20 @@ function config_shell {
     git clone --quiet https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
 }
 
+info "Linking dotfiles..."
+
+unlink ${HOME}/.zshrc 
+bash ${DIR}/clean.sh > /dev/null
+bash ${DIR}/clean.sh > /dev/null
+
 # Make ${HOME}/.config and ${HOME}/.cargo if not present
 mkdir -p ${HOME}/.config
 mkdir -p ${HOME}/.cargo
 
-# SymLink dotfiles and configs
-symlink ${DIR}/bash_aliases ${HOME}/.bash_aliases
-symlink ${DIR}/zshrc ${HOME}/.zshrc
-
 map_symlinks
+
 map_aliases
 map_local_bins
 config_shell
-unlink ${HOME}/.install.sh 2>/dev/null
 
-info "dotfile installation complete"
+success "Dotfile linking complete"
--- zshrc
+++ zshrc
@@ -1,3 +1,13 @@
+########## Dotfiles ##########
+function append_path() {
+    local paths=( "${1}" )
+
+    # If path substring is not in current env PATH, then append
+    for p in "${paths[@]}"; do
+        [[ ! "${PATH}" == *":${p}:"* ]] && export PATH="${p}:${PATH}"
+    done
+}
+
 export ZSH="${HOME}/.oh-my-zsh"
 source "${ZSH}/oh-my-zsh.sh"
 
@@ -6,35 +16,39 @@ source "${ZSH}/oh-my-zsh.sh"
 #source "${ZSH}/completion.zsh"    # tab-completion
 #source "${ZSH}/key-bindings.zsh"  # conditional history (e.g., git <UP_KEY>)
 
-export STARSHIP_CONFIG=${HOME}/.config/starship.toml
+export GOPATH="${HOME}/go"
+export GOROOT="/usr/local/opt/go/libexec"
+export GOOS="`uname | awk '{print tolower($0)}'`"
 
-export GOPATH=$HOME/go
-export GOROOT=/usr/local/opt/go/libexec
-export GOOS=`uname | awk '{print tolower($0)}'`
+# Set up shell aliases
+[ -f "${HOME}/.bash_aliases" ]     &&  source "${HOME}/.bash_aliases"
+[ -f "${HOME}/.cargo/env" ]        &&  source "${HOME}/.cargo/env"
+[ -f "${HOME}/.fzf.zsh" ]          &&  source "${HOME}/.fzf.zsh"
+#[ -s "${HOME}/.wasmer/wasmer.sh" ] &&  source "${HOME}/.wasmer/wasmer.sh"
 
 # Install rust, if missing
-[[ ! -e `which cargo` ]] && curl --proto "=https" --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
-
-# Set up aliases
-[ -f ${HOME}/.fzf.zsh ] && source ${HOME}/.fzf.zsh
-[ -f ${HOME}/.bash_aliases ] && source ${HOME}/.bash_aliases
-[ -f ${HOME}/.cargo/env ] && source ${HOME}/.cargo/env
+[[ ! -e `which cargo` ]] \
+    && curl --proto "=https" --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y
 
 # Install starship, if missing
-[[ ! -e `which starship` ]] && cargo install starship
+[[ ! -e `which starship` ]] \
+    && cargo install -j`nproc` starship
 
-# Add dotfiles bin to PATH
-export PATH=${PATH}:/usr/local/sbin                   
-export PATH=${PATH}:${HOME}/bin:${HOME}/bin/depot_tools
-export PATH=${PATH}:${HOME}/opt/sources/dotfiles/bin
-export PATH=${PATH}:${GOPATH}/bin:${GOROOT}/bin
+export STARSHIP_CONFIG="${HOME}/.config/starship.toml"
+
+# Prep shell env
+declare -a LOCAL_PATHS=( \
+    "/usr/local/bin" \
+    "/usr/local/sbin" \
+    "/opt/depot_tools" \
+    "${HOME}/opt/sources/dotfiles/bin" \
+    "${GOPATH}/bin" \
+    "${GOROOT}/bin" \
+    #"${HOME}/.wasmtime/bin" \
+) && append_path ${LOCAL_PATHS}
 
 # Terminal colors (runs base16 script)
 ${HOME}/.config/base16_color_space.sh
-
-# Wasmer
-#export WASMER_DIR="/Users/bynx/.wasmer"
-#[ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
 
 # Add zoxide to our shell
 # * Usage (https://github.com/ajeetdsouza/zoxide#examples):
@@ -47,7 +61,3 @@ ${HOME}/.config/base16_color_space.sh
 #     zr /foo     # remove /foo from the database
 eval "$(zoxide init posix --hook prompt)"
 eval "$(starship init zsh)"
-
-export WASMTIME_HOME="$HOME/.wasmtime"
-
-export PATH="$WASMTIME_HOME/bin:$PATH"
